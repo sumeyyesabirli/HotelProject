48ec364be96f3df3196b7d7823c556b228c21474
Sumeyye <sumeyyesabirli@gmail.com>
2023-08-11 01:00:09 +0300

    Güncellemeler

---
+++ HotelProject.PresentationLayer/ViewComponents/Default/_DefaultFeatureSection.cs
@@ -0,0 +1,21 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.PresentationLayer.ViewComponents.Default
+{
+    public class _DefaultFeatureSection : ViewComponent
+    {
+      private readonly IRoomDetailService _roomDetailService;
+     
+      public _DefaultFeatureSection(IRoomDetailService roomDetailService)
+      {
+          _roomDetailService = roomDetailService;
+      }
+     
+      public IViewComponentResult Invoke()
+      {
+          var values = _roomDetailService.TGetList();
+          return View(values);
+      }
+    }
+}
---
+++ HotelProject.PresentationLayer/ViewComponents/Default/_RoomRoomsSection.cs
@@ -0,0 +1,22 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.PresentationLayer.ViewComponents.Default
+{
+    public class _RoomRoomsSection : ViewComponent
+    {
+        private readonly IRoomDetailService _roomDetailService;
+
+        public _RoomRoomsSection(IRoomDetailService roomDetailService)
+        {
+            _roomDetailService = roomDetailService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _roomDetailService.TGetList();
+            return View(values);
+        }
+    }
+    
+}
